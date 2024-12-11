2b47250ab20b1d7a71f683794a5cd94f3a048af6
rongpengli <10457822+rongpengli@users.noreply.github.com>
2024-12-11 16:01:33 +0800

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,10 +1,4 @@
 export const feeds = [
-  // Daring Fireball
-  'https://daringfireball.net/feeds/main',
-
-  // JakeArchibald.com
-  'https://jakearchibald.com/posts.rss',
-
-  // Appjeniksaan
-  'https://appjeniksaan.nl/feed.xml',
+  // Zotero
+  'https://api.zotero.org/groups/4629889/items/top?&format=atom&key=prYGBRCK2G9KiIg6l2CUsjT7',
 ]
